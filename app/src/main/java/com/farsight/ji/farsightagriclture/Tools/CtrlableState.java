d649affe139f7a57afdebccaad513fb7c4729705
jiyangkang <jiyk@farsight.com.cn>
2015-12-06 17:04:56 +0800

    2015-12-05正式第一版

--- app/src/main/java/com/farsight/ji/farsightagriclture/Tools/CtrlableState.java
+++ app/src/main/java/com/farsight/ji/farsightagriclture/Tools/CtrlableState.java
@@ -24,13 +24,11 @@ public class CtrlableState extends View {
     private Context mContext;
 
     private Paint mPaint;
-    private Rect rectOri, rectDst, rectOpenOri, rectOpenDst, rectCloseOri, rectCloseDst;
+    private Rect rectOri, rectDst, rectScrollOri, rectScrollDst;
 
     private Bitmap bitmapdefault;
-    private Bitmap openDefault, closeDefault, openButton, closeButton, openButtonClicked,
-            closeButtonClicked;
-    private String name;
-    private int state = 0;
+    private Bitmap bitmapScroll;
+    private int state = -9999;
     private String stringState;
 
     private byte[] openCmd = new byte[7];
@@ -40,37 +38,37 @@ public class CtrlableState extends View {
 
         switch (type) {
             case NodeInfo.TYPE_WARM:
-                name = NodeInfo.WARM;
+//                name = NodeInfo.WARM;
                 openCmd = NodeInfo.OPEN_WARM;
                 closeCmd = NodeInfo.CLOSE_WARM;
                 break;
             case NodeInfo.TYPE_HUMIFY:
-                name = NodeInfo.HUMIFY;
+//                name = NodeInfo.HUMIFY;
                 openCmd = NodeInfo.OPEN_HUMIFY;
                 closeCmd = NodeInfo.CLOSE_HUMIFY;
                 break;
             case NodeInfo.TYPE_FAN:
-                name = NodeInfo.FAN;
+//                name = NodeInfo.FAN;
                 openCmd = NodeInfo.OPEN_FAN;
                 closeCmd = NodeInfo.CLOSE_FAN;
                 break;
             case NodeInfo.TYPE_LAMP:
-                name = NodeInfo.LAMP;
+//                name = NodeInfo.LAMP;
                 openCmd = NodeInfo.OPEN_LAMP;
                 closeCmd = NodeInfo.CLOSE_LAMP;
                 break;
             case NodeInfo.TYPE_DRENCHING:
-                name = NodeInfo.DRENCHING;
+//                name = NodeInfo.DRENCHING;
                 openCmd = NodeInfo.OPEN_DRENCHING;
                 closeCmd = NodeInfo.CLOSE_DRENCHING;
                 break;
             case NodeInfo.TYPE_SHADE:
-                name = NodeInfo.SHADE;
+//                name = NodeInfo.SHADE;
                 openCmd = NodeInfo.OPEN_SHADE;
                 closeCmd = NodeInfo.CLOSE_SHADE;
                 break;
             case NodeInfo.TYPE_ALARM:
-                name = NodeInfo.ALARM;
+//                name = NodeInfo.ALARM;
                 openCmd = NodeInfo.OPEN_ALARM;
                 closeCmd = NodeInfo.CLOSE_ALARM;
                 break;
@@ -80,16 +78,16 @@ public class CtrlableState extends View {
     public void setState(int state) {
         this.state = state;
         if (state == 1) {
-            openDefault = openButtonClicked;
-            closeDefault = closeButton;
+//            openDefault = openButtonClicked;
+//            closeDefault = closeButton;
             setStringState("\"开\"");
         } else if (state == 0) {
-            openDefault = openButton;
-            closeDefault = closeButtonClicked;
+//            openDefault = openButton;
+//            closeDefault = closeButtonClicked;
             setStringState("\"关\"");
         } else if (state == -1) {
-            openDefault = openButton;
-            closeDefault = closeButtonClicked;
+//            openDefault = openButton;
+//            closeDefault = closeButtonClicked;
             setStringState("断开连接");
         } else {
             setStringState("未连接");
@@ -117,12 +115,11 @@ public class CtrlableState extends View {
 
         mContext = context;
 
-        name = " ";
-
+//        name = " ";
         int widthMetrics = context.getResources().getDisplayMetrics().widthPixels;
 
         mPaint = new Paint();
-        mPaint.setTextSize(16);
+        mPaint.setTextSize(12);
         mPaint.setColor(Color.BLACK);
         mPaint.setAntiAlias(true);
         mPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
@@ -132,27 +129,19 @@ public class CtrlableState extends View {
         oX = bitmapdefault.getWidth();
         oY = bitmapdefault.getHeight();
         rectOri = new Rect(0, 0, oX, oY);
-        dX = widthMetrics / 12;
+        dX = widthMetrics / 7;
         dY = oY * dX / oX;
         rectDst = new Rect(0, 0, dX, dY);
 
-        int oBX, oBY, dBX, dBY;
-        openButton = BitmapFactory.decodeResource(getResources(), R.drawable.open);
-        openButtonClicked = BitmapFactory.decodeResource(getResources(), R.drawable.open_clicked);
-        oBX = openButton.getWidth();
-        oBY = openButton.getHeight();
-        dBX = rectDst.width();
-        dBY = oBY * dBX / oBX;
-        openDefault = openButton;
-        rectOpenOri = new Rect(0, 0, oBX, oBY);
-        rectOpenDst = new Rect(rectDst.right, rectDst.height() / 2, rectDst.right + dBX, rectDst.height() / 2 + dBY);
+        bitmapScroll = BitmapFactory.decodeResource(getResources(), R.drawable.scroll);
+        int sOX, sOY, sDY, sDX;
+        sOX = bitmapScroll.getWidth();
+        sOY = bitmapScroll.getHeight();
+        rectScrollOri = new Rect(0, 0, sOX, sOY);
 
-        closeButton = BitmapFactory.decodeResource(getResources(), R.drawable.close);
-        closeButtonClicked = BitmapFactory.decodeResource(getResources(), R.drawable.close_clicked);
-        closeDefault = closeButton;
-        rectCloseOri = new Rect(0, 0, closeDefault.getWidth(), closeDefault.getHeight());
-        rectCloseDst = new Rect(rectOpenDst.right, rectDst.height() / 2, rectOpenDst.right + dBX,
-                rectDst.height() / 2 + dBY);
+        sDX = sOX * dX / oX;
+        sDY = sOY * dY / oY;
+        rectScrollDst = new Rect(rectDst.left + 2, rectDst.bottom - sDY - 2, rectDst.left + sDX, rectDst.bottom -2);
 
         openCmd = NodeInfo.OPEN;
         closeCmd = NodeInfo.CLOSE;
@@ -162,14 +151,15 @@ public class CtrlableState extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawBitmap(bitmapdefault, rectOri, rectDst, mPaint);
-        canvas.drawBitmap(openDefault, rectOpenOri, rectOpenDst, mPaint);
-        canvas.drawBitmap(closeDefault, rectCloseOri, rectCloseDst, mPaint);
+//        canvas.drawBitmap(openDefault, rectOpenOri, rectOpenDst, mPaint);
+//        canvas.drawBitmap(closeDefault, rectCloseOri, rectCloseDst, mPaint);
 
-        if (name != null) {
-            canvas.drawText(name, rectOpenDst.left, rectDst.height() / 3, mPaint);
-        }
-        if (stringState != null) {
-            canvas.drawText(stringState, rectOpenDst.right + 1, rectDst.height() / 3, mPaint);
+//        if (name != null) {
+//            canvas.drawText(name, rectOpenDst.left, rectDst.height() / 3, mPaint);
+//        }
+        if (state > -1) {
+            canvas.drawBitmap(bitmapScroll, rectScrollOri, rectScrollDst, mPaint);
+            canvas.drawText(stringState, rectDst.width() *8 / 10, rectDst.height()*22 / 30, mPaint);
         }
 
     }
@@ -185,7 +175,7 @@ public class CtrlableState extends View {
         if (widthMode == MeasureSpec.EXACTLY) {
             width = widthSize;
         } else {
-            int desired = (int) (getPaddingLeft() + getPaddingRight() + rectDst.width() + 2 * rectOpenDst.width());
+            int desired = (int) (getPaddingLeft() + getPaddingRight() + rectDst.width());
             width = desired;
         }
 
@@ -204,41 +194,23 @@ public class CtrlableState extends View {
         float y = event.getY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (x > rectOpenDst.left && x < rectOpenDst.right && y > rectOpenDst.top && y < rectOpenDst.bottom) {
+                if (x > rectScrollDst.left && x < rectScrollDst.right && y > rectScrollDst.top &&
+                        y < rectScrollDst.bottom) {
                     if (state == 0) {
-                        openDefault = openButtonClicked;
+//                        openDefault = openButtonClicked;
                     }
-                } else if (x > rectCloseDst.left && x < rectCloseDst.right && y > rectCloseDst.top && y < rectCloseDst
-                        .bottom) {
+                } else if (x > rectScrollDst.left && x < rectScrollDst.right && y > rectScrollDst.top && y <
+                        rectScrollDst.bottom) {
                     if (state == 1) {
-                        closeDefault = closeButtonClicked;
+//                        closeDefault = closeButtonClicked;
                     }
                 }
                 break;
-            case MotionEvent.ACTION_UP:
-                if (x > rectOpenDst.left && x < rectOpenDst.right && y > rectOpenDst.top && y < rectOpenDst.bottom) {
+            case MotionEvent.ACTION_MOVE:
 
-                    if (state == 0) {
-                        openDefault = openButton;
-                        try {
-                            TotalDatas.qSend.put(openCmd);
-                            Log.d("put", StringTool.hexToString(openCmd) + name);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                } else if (x > rectCloseDst.left && x < rectCloseDst.right && y > rectCloseDst.top && y < rectCloseDst
-                        .bottom) {
-                    if (state == 1) {
-                        closeDefault = closeButton;
-                        try {
-                            TotalDatas.qSend.put(closeCmd);
-                            Log.d("put", StringTool.hexToString(closeCmd) + name);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+                break;
+            case MotionEvent.ACTION_UP:
+
                 break;
         }
         invalidate();
